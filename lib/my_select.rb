f9801ece059203e779aa3ad362561d1b47233692
Danielle Jasper <dnj547@live.com>
2019-03-08 23:07:02 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
  i = 0
  selected = []
  while i < collection.length
-   selected << yield(collection[i])
+   if yield(collection[i])
+     selected << collection[i]
    i += 1
  end
  selected
